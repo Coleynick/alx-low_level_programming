7f3071b2a385b7f09d9fad69ceddd23f08cef3cc
Coleynick <ezechidinma14@gmail.com>
2023-04-20 17:54:54 -0700

    preprocess

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main -initializes code
+ *
+ * return -shows a successful program
+ */
 int main (void)
 {
 	puts ("\"Programming is like building a multilingual puzzle");
