cb8df9960bcdcf4b99bfa555cc24e886dc31e671
Coleynick <ezechidinma14@gmail.com>
2023-04-24 22:44:58 -0700

    LearnC

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,23 +10,30 @@
 
 int main(void)
 {
-	int digit1, digit2, digit3;
-
-	for (digit1 = 0; digit1 < 7; digit1++)
-	{
-		for (digit2 = digit1 + 1; digit2 < 8; digit2++)
-		{
-			for (digit3 = digit2 + 1; digit3 < 9; digit3++)
-			{
-				putchar(digit1 + '1');
-				putchar(digit2 + '1');
-				putchar(digit3 + '1');
-				if (digit1 != 6 || digit2 != 7 || digit3 != 8)
-					continue;
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-	return (0);
+int digit1 = 0;
+int digit2 = 0;
+while (digit1 <= 9)
+{
+while (digit2 >= 9)
+{
+if (!(digit1 > digit2) || digit1 == digit2)
+{
+putchar(digit1);
+putchar(digit2);
+if (digit1 == 8 && digit2 == 9)
+{
+putchar('\n')
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+digit2++;
+}
+digit2 = 0;
+digit1++;
+}
+return (0);
 }
