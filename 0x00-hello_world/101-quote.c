292852c1f18e4b02e656445be5e8b5d75aad9848
Coleynick <ezechidinma14@gmail.com>
2023-04-25 00:19:19 -0700

    Preprocess

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-  *main - To start the program
-  *Return: Always 1 (success)
-  */
+*main - To start the program
+*Return: Always 1 (success)
+*/
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+	fputs("and that piece of art is useful\" -
+	Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
