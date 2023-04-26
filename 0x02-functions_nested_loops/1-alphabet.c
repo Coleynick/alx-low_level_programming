c52c64ce8bc49f386201c7796ca3c0991f4c3f5d
Coleynick <ezechidinma14@gmail.com>
2023-04-25 19:46:34 -0700

    Print putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point  of the program
+ *
+ * Return: Always 0
+ *
+ * print_alphabet - Function that prints lowercase alphabets
+ *
+ * alpha - Variable for the lowercase alphabet
+ */
+void print_alphabet(char alpha);
+int main(void)
+{
+	print_alphabet('a');
+	return (0);
+}
+/**
+ * print_alphabet - Function that prints lowercase alphabets
+ *
+ * alpha - Variable for the lowercase alphabet
+ */
+void print_alphabet(char alpha)
+{
+	putchar('alpha\n');
+}
