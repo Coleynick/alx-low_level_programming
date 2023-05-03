338b7a3e5e5763b1ad25299e71b4a05294e044dd
Coleynick <ezechidinma14@gmail.com>
2023-05-02 20:35:49 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+*print_rev-printsastringinreverse,followedbyanewline
+*@s:pointertothestringtoprint
+*/
+void print_rev(char *s)
+{
+int len = strlen(s);
+int i;
+
+for (i = len - 1; i >= 0; i--)
+_putchar(s[i]);
+_putchar('\n');
+}
