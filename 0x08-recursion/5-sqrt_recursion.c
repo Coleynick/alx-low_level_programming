6b17c9abb1bff340534404d89570ce0a6cab510b
Coleynick <ezechidinma14@gmail.com>
2023-05-11 22:19:29 -0700

    Square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,15 +3,30 @@
  * @n: the number to find the root of
  * Return: The square root or -1 if none
  */
+int _sqrt_helper(int n, int a);
 int _sqrt_recursion(int n)
 {
 if (n < 0)
 {
 return (-1);
 }
-else if (n == 0)
+else
 {
-return (n);
+return (_sqrt_helper(n, 1));
+}
+}
+int _sqrt_helper(int n, int a)
+{
+if (a * a > n)
+{
+return (-1);
+}
+else if (a * a == n)
+{
+return (a);
 }
 else
-return (_sqrt_helper(n, 1, n));
+{
+return (_sqrt_helper(n, a + 1));
+}
+}
