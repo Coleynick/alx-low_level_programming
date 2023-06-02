c476c11b34350753b0e8c874c81aecc6a09afc9c
Coleynick <ezechidinma14@gmail.com>
2023-06-02 03:51:35 -0700

    Printing two lines before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,8 @@ void beforeMain(void) __attribute__((constructor));
  */
 void beforeMain(void)
 {
-char *g = "You're"
-char h[10] = "beat!"
-printf("%s %s and yet, you must allow,\nI bore my house upon my back!\n", g, h);
+char *g = "You're";
+char h[10] = "beat!";
+printf("%s %s and yet, you must allow,\n", g, h);
+printf("I bore my house upon my back!\n");
 }
