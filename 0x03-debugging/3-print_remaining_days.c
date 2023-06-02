ea97e6ffeeefef9a47c27543193aa308ebeff05f
Coleynick <ezechidinma14@gmail.com>
2023-06-02 09:56:39 -0700

    Fixing error to produce correctly all dates and leap years

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,24 +9,38 @@
 * @year: year
 * Return: void
 */
-
 void print_remaining_days(int month, int day, int year)
 {
-if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+int is_leap_year = 0;
+int day_of_year = convert_day(month, day);
+
+if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 {
-if (month > 2 || (month == 2 && day == 29))
+is_leap_year = 1;
+}
+
+if (month == 2 && day == 29)
 {
-printf("Day of the year: %d\n", day + 1);
-printf("Remaining days: %d\n", 366 - (day + 1));
+if (is_leap_year)
+{
+printf("Day of the year: %d\n", day_of_year);
+printf("Remaining days: %d\n", 366 - day_of_year);
+return;
 }
 else
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+return;
 }
 }
-else
+
+if (month > 2 && is_leap_year)
 {
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 365 - day);
+day_of_year++;
 }
+
+printf("Day of the year: %d\n", day_of_year);
+printf("Remaining days: ");
+printf("%d\n", is_leap_year ? 366 - day_of_year : 365 - day_of_year);
 }
+
