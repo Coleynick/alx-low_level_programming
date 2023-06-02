fb2fe2a9ae15ae1e352a6abbe3b049d9368aec77
Coleynick <ezechidinma14@gmail.com>
2023-06-02 09:46:08 -0700

    Fixing error to produce correctly all dates and leap years

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,9 +12,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-if (year % 4 == 0 && (year % 400 == 0 || year % 100 != 0))
+if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
 {
-if (month > 2 || (month == 2 && day >= 29))
+if (month > 2 || (month == 2 && day == 29))
 {
 printf("Day of the year: %d\n", day + 1);
 printf("Remaining days: %d\n", 366 - day - 1);
