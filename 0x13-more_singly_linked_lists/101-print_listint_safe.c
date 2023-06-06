864bdd92c2f29a7860cf80564490898badc43498
Coleynick <ezechidinma14@gmail.com>
2023-06-05 19:02:57 -0700

    Printing a listint_t linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+#include <stddef.h>
 
 /**
 * print_listint_safe - Prints a list
@@ -13,21 +14,41 @@ size_t print_listint_safe(const listint_t *head)
 const listint_t *nextNode = head;
 const listint_t *nextTwoNode = head;
 size_t nc_count = 0;
+int loopDetected = 0;
 
+nextNode = nextTwoNode = head;
 while (nextNode && nextTwoNode && nextTwoNode->next)
 {
 nextNode = nextNode->next;
 nextTwoNode = nextTwoNode->next->next;
-
 if (nextNode == nextTwoNode)
 {
-printf("-> [%p] %d\n", (void *) nextTwoNode, nextTwoNode->n);
-exit(98);
+loopDetected = 1;
+break;
 }
-
-printf("[%p] %d\n", (void *) nextNode, nextNode->n);
+}
+if (loopDetected)
+{
+nextNode = head;
+while (nextNode != nextTwoNode)
+{
+printf("[%p] %d\n", (void *)nextNode, nextNode->n);
+nc_count++;
+nextNode = nextNode->next;
+nextTwoNode = nextTwoNode->next;
+}
+printf("[%p] %d\n", (void *)nextNode, nextNode->n);
+nc_count++;
+printf("-> [%p] %d\n", (void *)nextTwoNode, nextTwoNode->n);
 nc_count++;
 }
-
+else
+{
+while (head != NULL)
+{
+nc_count += printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+}
+}
 return (nc_count);
 }
