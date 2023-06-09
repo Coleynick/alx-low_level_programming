6a0577a6940c5080347720d425c6771612e2056b
Coleynick <ezechidinma14@gmail.com>
2023-06-08 18:26:01 -0700

    Return the number of bits you would need to flip to get from one number to another

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+/**
+* flip_bits - Returns d butNum needed to flip to get from 1 num to another
+* @n: First num
+* @m: Second(2nd) num
+* Return: bitNum to flip
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int k = n ^ m;
+unsigned int bitNum = 0;
+int r = 0;
+
+while (k > r)
+{
+bitNum += k & 1;
+k >>= 1;
+}
+
+return (bitNum);
+}
