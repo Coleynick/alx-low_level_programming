f808e6c9ec1aec980b423e3469c6754567c97093
Coleynick <ezechidinma14@gmail.com>
2023-07-21 03:58:22 -0700

    Delete at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
 * delete_dnodeint_at_index - Deletes new node.
@@ -8,35 +9,30 @@
 */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-dlistint_t *currentNode = *head;
-unsigned int countNodes = 0;
+	dlistint_t *currentNode = *head;
+	unsigned int countNodes = 0;
 
-if (*head == NULL)
-return (-1);
-
-if (index == 0)
-{
-*head = (*head)->next;
-if (*head != NULL)
-(*head)->prev = NULL;
-free(currentNode);
-return (1);
-}
-while (currentNode != NULL && countNodes < index)
-{
-currentNode = currentNode->next;
-countNodes++;
-}
-
-if (currentNode == NULL)
-return (-1);
-
-if (currentNode->next != NULL)
-currentNode->next->prev = currentNode->prev;
-
-if (currentNode->prev != NULL)
-currentNode->prev->next = currentNode->next;
-
-free(currentNode);
-return (1);
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(currentNode);
+		return (1);
+	}
+	while (currentNode != NULL && countNodes < index)
+	{
+		currentNode = currentNode->next;
+		countNodes++;
+	}
+	if (currentNode == NULL)
+		return (-1);
+	if (currentNode->next != NULL)
+		currentNode->next->prev = currentNode->prev;
+	if (currentNode->prev != NULL)
+		currentNode->prev->next = currentNode->next;
+	free(currentNode);
+	return (1);
 }
