c377cfbec99272ba15b4caa40e77c852a2adad26
Coleynick <ezechidinma14@gmail.com>
2023-07-20 19:23:21 -0700

    Return the number of elements in a linked dlistint_t list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,1 +1,19 @@
 #include "lists.h"
+
+/**
+* dlistint_len - Returns number of L-ments in a dlistint_t list.
+* @h: Points to head.
+* Return: Element number.
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodesCount = 0;
+
+	while (h != NULL)
+	{
+		nodesCount++;
+		h = h->next;
+	}
+
+	return (nodesCount);
+}
