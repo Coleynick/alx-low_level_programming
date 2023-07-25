5157ae3ad7138ccb72e2368463a51c98434688f6
Coleynick <ezechidinma14@gmail.com>
2023-07-24 20:18:47 -0700

    Update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Coleynick/alx-low_level_programming/master/0x18-dynamic_libraries/101-make_me_win.so
-export LD_PRELOAD="$PWD/../101-make_me_win.so"
+LD_PRELOAD=./101-make_me_win.so ./gm 9 8 10 24 75 9
